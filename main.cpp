58bc23dc5e3c1d00a20a6159f62df3a423023a47
GuXiang <k168655299@163.com>
2026-03-12 03:35:36 +0000

    The content of main.cpp has been improved.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void sort(int arr[])
+void m_sort(int arr[], int len)
 {
-
+	for(int i = 0; i < len; i++)
+	{
+		for(int j = 0; j < len - i - 1; j++)
+		{
+			if(arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
 
 int main()
 {
 	int arr[] = {7,11,2,6,33,21};
-	sort(arr);
+	m_sort(arr, 6);
+	for(int i = 0; i < 6; i++)
+	{
+		cout << arr[i] << ' ';
+	}
 	return 0;
 }
